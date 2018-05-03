dceddd6e6a2cf18e27437c74705cffe5e6238ec2
Morgan Marshall <hkmarshall1@yahoo.com>
2018-05-03 16:15:25 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+  <ul id="dropdown1" class="dropdown-content">
+  <li><a href="#!">one</a></li>
+  <li><a href="#!">two</a></li>
+  <li class="divider"></li>
+  <li><a href="#!">three</a></li>
+</ul>
+<nav class="grey darken-3">
+  <div class="nav-wrapper">
+    <a href="#!" class="brand-logo">Logo</a>
+    <ul class="right hide-on-med-and-down">
+      <li><a href="sass.html">Sass</a></li>
+      <li><a href="badges.html">Components</a></li>
+      <!-- Dropdown Trigger -->
+      <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li>
+    </ul>
+  </div>
+</nav>
+<br><br>
+    <img width="25%" src="./assets/logo.png">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  },
+  mounted: function() {
+    M.AutoInit();
+  }
+};
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
